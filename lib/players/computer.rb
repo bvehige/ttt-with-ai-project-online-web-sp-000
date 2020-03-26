a101e44ab7984fed68936bb6b323745bcc824494
Brian Vehige <brianvehige@gmail.com>
2020-03-26 23:08:21 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,75 +20,74 @@ module Players
 #CORNERS = board.cells[0, 2, 6, 8]
 #MIDDLES = board.cells[1, 3, 5, 7]
 
-def move(board)
-  if center?
-    input = 5 
-  elsif winning_move != nil
-    input = winning_move + 1 
-  elsif block != nil
-    input = block + 1 
-  elsif corner_move != nil 
-    input = corner_move + 1 
-  elsif middle_move != nil 
-    input = middle_move + 1 
-  else
+def move(board, timer = 0)
+    @board = board
+    timer <= 0 ? @timer = 0.01 : @timer = timer
+    case
+      when winning_move != nil
+        input = winning_move + 1
+      when blocking_move != nil
+        input = blocking_move + 1
+      when center?
+        input = 5
+      when opposite_corner != nil && board.taken?(opposite_corner) == false
+        input = opposite_corner
+      when corner
+        input = corner + 1
+      else
         until !board.taken?(input)
           input = (1..9).to_a.sample
         end
     end
-
-end
-
-def computer_move 
-  winning_move || block || corner_move || middle_move 
-end
-
-def corner_move 
-  corner.find {|cell| board.cells[cell] == " "}
-end
-
-def middle_move
-  middle.find {|cell| board.cells[cell] == " "}
-end
-
-def block 
-  winning_row = WIN_COMBINATIONS.find do |tokens|
-      (board.cells[tokens[0]] == enemy && board.cells[tokens[1]] == enemy && board.cells[tokens[2]] == " ") || (board.cells[tokens[1]] == enemy && board.cells[combo[2]] == enemy && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == enemy && board.cells[tokens[0]] == enemy && board.cells[tokens[1]] == " ")
-    end
-    if winning_row != nil
-      winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+    sleep(@timer)
+    input.to_s
   end
-end
-
-def winning_move
-  winning_row = WIN_COMBINATIONS.find do |tokens|
-      (board.cells[tokens[0]] == token && board.cells[tokens[1]] == token && board.cells[tokens[2]] == " ") || (board.cells[tokens[2]] == token && board.cells[tokens[1]] == token && board.cells[tokens[0]] == " ") || (board.cells[tokens[0]] == token && board.cells[tokens[2]] == token && board.cells[tokens[1]] == " ")
+  def other
+    token == "X" ? "O" : "X"
+  end
+  def winning_move
+    winning_row = WIN_COMBINATIONS.find do |combo|
+      (board.cells[combo[0]] == token && board.cells[combo[1]] == token && board.cells[combo[2]] == " ") || (board.cells[combo[2]] == token && board.cells[combo[1]] == token && board.cells[combo[0]] == " ") || (board.cells[combo[0]] == token && board.cells[combo[2]] == token && board.cells[combo[1]] == " ")
     end
     if winning_row != nil
       winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
     end
-end
-
-def center?
+  end
+  def blocking_move
+    winning_row = WIN_COMBINATIONS.find do |combo|
+      (board.cells[combo[0]] == other && board.cells[combo[1]] == other && board.cells[combo[2]] == " ") || (board.cells[combo[1]] == other && board.cells[combo[2]] == other && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == other && board.cells[combo[0]] == other && board.cells[combo[1]] == " ")
+    end
+    if winning_row != nil
+      winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+    end
+  end
+  def center?
     board.cells[4] == " "
   end
-
-def middle 
- MIDDLES   
+  def corners
+    CORNERS.shuffle!
+  end
+  def corner
+    corners.find {|corner| board.cells[corner] == " "}
+  end
+  def opposite_corner
+    case
+      when board.taken?(1) && !board.taken?(9)
+        9
+      when board.taken?(9) && !board.taken?(1)
+        1
+      when board.taken?(3) && !board.taken?(7)
+        7
+      when board.taken?(7) && !board.taken?(3)
+        3
+      else
+        nil
+    end
+  end
 end
 
-def corner 
-  CORNERS 
-end
-
-def enemy
-  token = "X"? "O" : "X"
-end
 
 
 
 
-  
-end
 
-end
