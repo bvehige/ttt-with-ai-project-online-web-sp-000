01c9dc551a3cfeec739965a520f5e47e18076d3d
Brian Vehige <brianvehige@gmail.com>
2020-03-26 23:09:28 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,3 +20,75 @@ module Players
 #CORNERS = board.cells[0, 2, 6, 8]
 #MIDDLES = board.cells[1, 3, 5, 7]
 
+def move(board)
+  if center?
+    input = 5 
+  elsif winning_move != nil
+    input = winning_move + 1 
+  elsif block != nil
+    input = block + 1 
+  elsif corner_move != nil 
+    input = corner_move + 1 
+  elsif middle_move != nil 
+    input = middle_move + 1 
+  else
+        until !board.taken?(input)
+          input = (1..9).to_a.sample
+        end
+    end
+
+end
+
+def computer_move 
+  winning_move || block || corner_move || middle_move 
+end
+
+def corner_move 
+  corner.find {|cell| board.cells[cell] == " "}
+end
+
+def middle_move
+  middle.find {|cell| board.cells[cell] == " "}
+end
+
+def block 
+  winning_row = WIN_COMBINATIONS.find do |tokens|
+      (board.cells[tokens[0]] == enemy && board.cells[tokens[1]] == enemy && board.cells[tokens[2]] == " ") || (board.cells[tokens[1]] == enemy && board.cells[combo[2]] == enemy && board.cells[combo[0]] == " ") || (board.cells[combo[2]] == enemy && board.cells[tokens[0]] == enemy && board.cells[tokens[1]] == " ")
+    end
+    if winning_row != nil
+      winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+  end
+end
+
+def winning_move
+  winning_row = WIN_COMBINATIONS.find do |tokens|
+      (board.cells[tokens[0]] == token && board.cells[tokens[1]] == token && board.cells[tokens[2]] == " ") || (board.cells[tokens[2]] == token && board.cells[tokens[1]] == token && board.cells[tokens[0]] == " ") || (board.cells[tokens[0]] == token && board.cells[tokens[2]] == token && board.cells[tokens[1]] == " ")
+    end
+    if winning_row != nil
+      winning_cell = winning_row.find {|cell| board.cells[cell] == " "}
+    end
+end
+
+def center?
+    board.cells[4] == " "
+  end
+
+def middle 
+ MIDDLES   
+end
+
+def corner 
+  CORNERS 
+end
+
+def enemy
+  token = "X"? "O" : "X"
+end
+
+
+
+
+  
+end
+
+end
