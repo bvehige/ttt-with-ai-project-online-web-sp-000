bf69c1ea0dc6a59b66030f7ff0f9d769126a0f3f
Brian Vehige <brianvehige@gmail.com>
2020-03-26 22:20:10 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,12 +5,10 @@ module Players
 
 attr_accessor :board
 
-CORNERS = board.cells[0, 2, 6, 8]
-MIDDLES = board.cells[1, 3, 5, 7]
+#CORNERS = board.cells[0, 2, 6, 8]
+#MIDDLES = board.cells[1, 3, 5, 7]
 
 def move(board)
-  @board = board
-  binding.pry 
   if board.cells[4] == " "
    "5"
   elsif corners.find {|cell| board.cells[cell] == " "}
