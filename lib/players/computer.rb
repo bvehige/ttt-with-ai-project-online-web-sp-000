a80171fc84bc7f464f6ea1af2f10d1d35ff563a9
Brian Vehige <brianvehige@gmail.com>
2020-03-26 23:10:23 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -21,6 +21,30 @@ module Players
 #MIDDLES = board.cells[1, 3, 5, 7]
 
 def move(board)
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[6] == " "
+        "7"
+      elsif board.cells[8] == " "
+        "9"
+      elsif board.cells[1] == " "
+        "2"
+      elsif board.cells[3] == " "
+        "4"
+      elsif board.cells[5] == " "
+        "6"
+      elsif board.cells[7] == " "
+        "8"
+      end
+
+
+
+
+def move(board)
   if center?
     input = 5 
   elsif winning_move != nil
